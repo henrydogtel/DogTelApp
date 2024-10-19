0ce4ea71bc03bfc075cd36442919f82d3ef99f46
Lucas21705 <lmmcyrus3@gmail.com>
2024-10-18 22:15:34 -0300

    ad

--- client/src/components/LoginForm/index.tsx
+++ client/src/components/LoginForm/index.tsx
@@ -91,7 +91,7 @@ function SignInForm() {
           required
         />
         <label className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-          Email Address
+          Email 
         </label>
         {touched.email && errors.email && (
           <span className="text-red-500 text-xs mt-1">{errors.email}</span>
