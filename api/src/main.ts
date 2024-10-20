8535edf1c5c6e59cc65b093300012eaec3a67622
omaryesithvilladiego <omaryesithvilladiegocarrascal@outlook.com>
2024-10-20 08:31:39 -0400

    configurar swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './helpers/developmentEnv';
-
+import {DocumentBuilder,SwaggerModule} from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('NestJS Api Dogtel')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app,swaggerOptions)
+  SwaggerModule.setup('api',app,document)
   await app.listen(PORT ?? 3000);
 }
 
