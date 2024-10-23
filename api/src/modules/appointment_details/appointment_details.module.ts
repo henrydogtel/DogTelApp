45bd8fcd6d01fc08b73d32a2f857b3db5b1f9926
Luchi Elipinali <luelipinali@gmail.com>
2024-10-23 17:06:47 -0300

    eliminacion de api del pull request

--- api/src/modules/appointment_details/appointment_details.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppointmentDetailsService } from './appointment_details.service';
-import { AppointmentDetailsResolver } from './appointment_details.resolver';
-
-@Module({
-  providers: [AppointmentDetailsResolver, AppointmentDetailsService],
-})
-export class AppointmentDetailsModule {}
