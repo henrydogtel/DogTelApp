7f227c61fc17ca1de53a02bec139a4886e87f255
Luchi Elipinali <luelipinali@gmail.com>
2024-10-23 16:03:41 -0300

    registro de owner con back incluido

--- client/src/app/lib/server/fetchUsers.ts
+++ client/src/app/lib/server/fetchUsers.ts
@@ -41,43 +41,46 @@ export const postSignUpSitter = async (user: IRegisterUser) => {
   }
 };
 export const postSignUpOwner = async (user: IRegisterUser) => {
-  try {
-    const response = await fetch("http://", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    });
 
-    if (!response.ok) {
-      const errorMessage = `Error ${response.status}: ${response.statusText}`;
-      throw new Error(errorMessage);
-    }
-    if (response.ok) {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.onmouseenter = Swal.stopTimer;
-          toast.onmouseleave = Swal.resumeTimer;
-        },
-      });
-      Toast.fire({
-        icon: "success",
-        title: "User registered in successfully",
-      });
-    }
+ // Garantizamos que el rol siempre sea "user"
+ const userWithRole = {
+  ...user,
+  role: "user",
+};
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error during sign-up:", error);
-    throw new Error("Failed to sign up. Please try again.");
-  }
+const query = JSON.stringify({
+  query: `mutation CreateUser($firstname: String!, $lastname: String!, $birthdate: DateTime!, $address: String!, $role: String!, $password: String!, $email: String!) {
+    createUser(
+      firstname: $firstname,
+      lastname: $lastname,
+      birthdate: $birthdate,
+      address: $address,
+      role: $role,
+      password: $password,
+      email: $email
+    ) {
+      firstname
+      lastname
+      birthdate
+      address
+      role
+      credentials {
+        password
+        email
+      }
+    }
+  }`,
+  variables: userWithRole, // Aquí enviamos el usuario con el rol incluido
+});
+
+const response = await fetch('http://localhost:3001/graphql', {
+  headers: { 'Content-Type': 'application/json' },
+  method: 'POST',
+  body: query,
+});
+
+return response;
+
 };
 
 export const postSignIn = async (credentials: ILoginUser) => {
