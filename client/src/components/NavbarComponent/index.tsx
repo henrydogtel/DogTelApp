1295444f69ecb9f53c52e128be62e23a7614be26
omaryesithvilladiego <omaryesithvilladiegocarrascal@outlook.com>
2024-11-04 13:47:18 -0500

    upload image cloudinary, changes to dev-frontend

--- client/src/components/NavbarComponent/index.tsx
+++ client/src/components/NavbarComponent/index.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import Image from "next/image";
 import Link from "next/link";
@@ -6,32 +6,117 @@ import React, { useContext, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import { UserContext } from "@/context/user";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { neucha } from "@/app/lib/server/fonts";
-import { signOut } from 'next-auth/react';
+import Shepherd from "shepherd.js";
+import 'shepherd.js/dist/css/shepherd.css';
+import './shepherd.css'; // Asegúrate de importar tus estilos personalizados
 
 const NavbarComponent = () => {
   const { logOut, user } = useContext(UserContext);
+  const pathname = usePathname();
   const router = useRouter();
-  const [userLocal,setUser] = useState<string | null>(null)
-  const [token,setToken] = useState<string | null>(null)
-  const [role, setRole] = useState<string | null>(null)
-
+  const [userLocal, setUser] = useState<string | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+  const [role, setRole] = useState<string | null>(null);
 
   const logOutUser = () => {
     logOut();
   };
 
-  // Verifica si el usuario y el token existen en localStorage
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const storedUser = localStorage.getItem('user');
+      const storedToken = localStorage.getItem('token');
+
+      setUser(storedUser);
+      if (storedUser) {
+        const userParse = JSON.parse(storedUser);
+        setRole(userParse.role);
+      }
+      setToken(storedToken);
+    }
+  }, [user]);
+
+  const startTour = () => {
+    const tour = new Shepherd.Tour({
+      useModalOverlay: true,
+      defaultStepOptions: {
+        classes: 'shepherd-step-custom', // Clase CSS personalizada
+        scrollTo: true,
+      },
+    });
+
+    if (userLocal && role === 'user') {
+      tour.addStep({
+        id: 'find-sitters',
+        title: 'Find Sitters',
+        text: 'Here you can find sitters to take care of your dog.',
+        attachTo: {
+          element: '.find-sitters',
+          on: 'bottom',
+        },
+        buttons: [
+          {
+            text: 'Next',
+            action: tour.next,
+          },
+        ],
+      });
+    }
+
+    if (userLocal && token) {
+      tour.addStep({
+        id: 'dashboard',
+        title: 'Dashboard',
+        text: 'Go to your Dashboard to add pets and manage your profile.',
+        attachTo: {
+          element: '.dashboard-button',
+          on: 'bottom',
+        },
+        buttons: [
+          {
+            text: 'Next',
+            action: tour.next,
+          },
+        ],
+      });
+    }
+
+    if (userLocal && token) {
+      tour.addStep({
+        id: 'sign-out',
+        title: 'Sign Out',
+        text: 'Click here to log out.',
+        attachTo: {
+          element: '.sign-out-button',
+          on: 'bottom',
+        },
+        buttons: [
+          {
+            text: 'Done',
+            action: function() {
+              tour.complete();
+              window.scrollTo({
+                top: 0,
+                behavior: 'smooth' // Desplazamiento suave hacia arriba
+              });
+            },
+          },
+        ],
+      });
+    }
+
+  
+
+    tour.start();
+  };
 
   useEffect(() => {
-    setUser(localStorage.getItem('user'));
-    let userParse = JSON.parse(String(localStorage.getItem('user')))
-    if(userParse) setRole(userParse.role);
-    setToken(localStorage.getItem('token')) ;
-  },[user])
-  
- 
+    if (pathname === '/home') {
+      startTour();
+    }
+  }, [userLocal, token]);
 
   return (
     <div>
@@ -50,23 +135,19 @@ const NavbarComponent = () => {
           </Link>
 
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-
             <ul className={`${neucha.className} font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-[#D5E1DD] md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-[#ffb64f26]`}>
+              {userLocal && role === 'user' && (
+                <li>
+                  <Link
+                    href="/sittersPricesDetail"
+                    className="block p-2 px-3 text-white bg-[#ffd735] hover:bg-[#ffbf52] rounded-2xl find-sitters"
+                    aria-current="page"
+                  >
+                    <span style={{ color: 'black' }}>Find Sitters</span>
+                  </Link>
+                </li>
+              )}
 
-
-            {userLocal && role === 'user' && <li>
-                
-                <Link
-                     href="sittersPricesDetail"
-                     className=" block p-2 px-3 text-white bg-[#ffd735] hover:bg-[#ffbf52] rounded-2xl"
-                     aria-current="page"
-                   >
-                    <span style={{color:'black'}}>Find Sitters</span> 
-                   </Link>
-                 </li>} 
-              
-
-              {/* Renderiza los botones de Sign In y Sign Up solo si no hay user y token */}
               {!userLocal || !token ? (
                 <>
                   <li>
@@ -91,25 +172,25 @@ const NavbarComponent = () => {
               ) : null}
 
               <Link
-                  href="/home"
+                href="/home"
                 className="bg-[#fc955e] hover:bg-[#d9865d] text-white font-semibold py-2 px-4 rounded-full shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-[#F0854F] focus:ring-opacity-75 transition duration-300 ease-in-out flex items-center justify-center"
               >
                 Home
               </Link>
 
               {userLocal && token && (
-                <Link
-                  href="/dashboard"
-                  className="bg-[#fc955e] hover:bg-[#d9865d] text-white font-semibold py-2 px-4 rounded-full shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-[#F0854F] focus:ring-opacity-75 transition duration-300 ease-in-out flex items-center justify-center"
+                <button
+                  onClick={() => router.push('/dashboard')}
+                  className="bg-[#fc955e] hover:bg-[#d9865d] text-white font-semibold py-2 px-4 rounded-full shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-[#F0854F] focus:ring-opacity-75 transition duration-300 ease-in-out flex items-center justify-center dashboard-button"
                 >
                   Dashboard
-                </Link>
+                </button>
               )}
 
               {userLocal && token && (
                 <button
-                  onClick={() => logOutUser()}
-                  className="bg-[#f8503a] hover:bg-[#c54534] text-white font-semibold py-2 px-4 rounded-full shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-[#F0854F] focus:ring-opacity-75 transition duration-300 ease-in-out flex items-center justify-center"
+                  onClick={logOutUser}
+                  className="bg-[#f8503a] hover:bg-[#c54534] text-white font-semibold py-2 px-4 rounded-full shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-[#F0854F] focus:ring-opacity-75 transition duration-300 ease-in-out flex items-center justify-center sign-out-button"
                 >
                   <FontAwesomeIcon icon={faSignOutAlt} size="sm" />
                 </button>
